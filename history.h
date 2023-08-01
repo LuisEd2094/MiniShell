6b308c03d84caf40d768e8e6af06d98420364edf
Luis <luissoto2094@gmail.com>
2023-08-01 10:18:57 +0200

    it now creates an initial history from a file, gives you just the last 1k entries

--- history.h
+++ history.h
@@ -8,4 +8,15 @@ typedef struct s_history
     t_list *max_current_history;
 } t_history;
 
+typedef enum s_order
+{
+    INIT,
+    UPDATE,
+    CLOSE,
+
+} h_order;
+
+# define MAX_H_SIZE 1000
+# define HISTORY_FILE "my_history.txt"
+
 #endif
--- main.c
+++ main.c
@@ -11,65 +11,115 @@ void remove_new_line(char *line)
     int i;
     
     i = 0;
-    while (line[i] != '\n')
+    while (line[i] != '\n' && line[i])
         i++;
     line[i] = '\0';
 }
 
-t_list    *ft_read_history(const char *file)
+t_list    *ft_read_history(int fd, t_list ** head, int *size)
 {
-    t_list  *node;
     t_list  *tmp;
     t_list  *new;
     char    *line;
-    int     fd;
 
-    fd = open(file, O_RDONLY);
     if (fd <= 0)
         return NULL;
     line = get_next_line(fd, 10);
     if (line)
     {
         remove_new_line(line);
-        node = ft_lstnew(line); // need to check line?
-        if (!node)
+        *(head) = ft_lstnew(line); // need to check line?
+        if (!*head)
             exit(1);
+        *(size) += 1;
     }
-    tmp = NULL;
-    node->next = tmp;
+    tmp = *head;
     while(line)
     {
         line = get_next_line(fd, 10);
         if (line)
         {
             remove_new_line(line);
-            tmp = ft_lstnew(line);
+            new = ft_lstnew(line);
             //printf("TEMPS %s\n", (char *)tmp->content);
-            if (!tmp)
+            if (!new)
                 exit(0);
-            tmp->next = tmp;
-            tmp = tmp->next;
+            *(size) += 1;
+            tmp->next = new;
+            (*head)->last = new; 
+            tmp = new;
         }
     }
     close(fd);
-    return (node);
 }
 
-void init_history()
+void add_to_history(t_list *node)
+{
+    t_list *temp;
+
+    temp = node;
+    while (temp)
+    {
+        add_history(temp->content);
+        temp = temp->next;
+    }
+}
+
+
+void    get_max_input(t_list *head, t_list **max_input, int size)
+{
+    t_list *temp;
+    int i;
+
+    i = size - MAX_H_SIZE;
+    printf("%i\n", i);
+    temp = head;
+    while (i-- >= 1)
+        temp = temp->next;
+    printf("%i %s\n", i, (char *)temp->content);
+    *(max_input) = temp;
+}
+
+void work_history(int order, char *input)
 {
     static t_list *head = NULL;
     static t_list *max_input = NULL;
+    static int  size = 0;
 
-    if (!head)
-        head = ft_read_history("my_history.txt");
+    if (order == INIT)
+    {
+        ft_read_history(open(HISTORY_FILE, O_RDONLY), &head, &size);
+        printf("size %i\n", size);
+        if (size > MAX_H_SIZE)
+            get_max_input(head, &max_input, size);
+        if (max_input)
+            add_to_history(max_input);
+        else if (head)
+            add_to_history(head);
+    }
+    else if (order = UPDATE)
+    {
+
+    }
     //history.head->next = ft_lstnew("adios");
 
+
+
+
+    /*
     t_list *temp = head;
     while (temp)
     {
-        printf(" %s\n", (char *)temp->content);
+        printf("size %i %s\n", size, (char *)temp->content);
         temp = temp->next;
     }
+    temp = max_input;
+    while (temp)
+    {
+        printf("%p %s\n",&(temp->content),  (char *)temp->content);
+        temp = temp->next;
+    }
+    printf("LAST %p %s\n",&(head->last->content), (char *)head->last->content);*/
 }
 
 int main() {
@@ -79,7 +129,7 @@ int main() {
     // Readline setup
 
     // Read previous history from a file
-    init_history();
+    work_history(INIT, NULL);
 
 
     // Main loop
@@ -93,7 +143,7 @@ int main() {
 
         if (input[0] != '\0') {
             // If the input is not empty, add it to the history
-            add_history(input);
+            work_history(UPDATE,input);
         }
 
         // Your processing logic here...
