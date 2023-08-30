458bb61d0a4de552f4bd0fb87e234994b06dd2f9
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-08-30 10:47:39 +0200

    Create MS_functions.h

---
+++ src/functions/MS_functions.h
@@ -0,0 +1,14 @@
+#ifndef MS_FUNCTIONS_H
+# define MS_FUNCTIONS_H
+
+#include "../../includes/ft_printf.h"
+#include "../../includes/libft.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_cd(char **arguments);
+int	ft_echo(char **arguments);
+int	ft_pwd(char **arguments);
+
+#endif
