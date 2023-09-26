a3038ed2901a657095c91c6a56da056b5ce37851
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-09-26 16:41:34 +0200

    Update pipe.c

--- src/pipe/pipe.c
+++ src/pipe/pipe.c
@@ -1,42 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-int	**malloc_pipe(int num_pipes)
-{
-	int	i;
-	int	**pipes;
-
-	pipes = (int **)malloc((num_pipes) * sizeof(int *));
-	if (pipes == NULL)
-	{
-		perror("Error en malloc");
-		return (NULL);
-	}
-	i = -1;
-	while (++i < num_pipes)
-	{
-		pipes[i] = (int *)malloc(2 * sizeof(int));
-		if (pipes[i] == NULL)
-		{
-			perror("Error en malloc");
-			return (NULL);
-		}
-	}
-	return (pipes);
-}
-
-void	free_pipe(int **pipes, int num_pipes)
-{
-	int	i;
-
-	i = -1;
-	while (++i < num_pipes)
-		free(pipes[i]);
-	free(pipes);
-}
+#include "MS_pipe.h"
 
 int	make_pipe(int **pipes, int num_pipes)
 {
