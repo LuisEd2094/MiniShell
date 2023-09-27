8cbbe6d60d434bbf8e6369984b87c7430885649e
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-09-27 10:48:38 +0200

    Update MS_pipe.h

--- src/pipe/MS_pipe.h
+++ src/pipe/MS_pipe.h
@@ -1,3 +1,6 @@
+#ifndef MS_FUNCTIONS_H
+# define MS_FUNCTIONS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,3 +10,8 @@
 /*	---	Memory pipe	---	*/
 int	**malloc_pipe(int num_pipes);
 void	free_pipe(int **pipes, int num_pipes);
+
+/*  ---  Pipe  ---  */
+int	ft_pipe(char ***commands, int num_pipes)
+
+#endif
