0d48e54502c1970c2aa9baea166978105f45cd19
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-09-27 15:23:25 +0200

    Update MS_pipe.h

--- src/pipe/MS_pipe.h
+++ src/pipe/MS_pipe.h
@@ -6,12 +6,13 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <minishell.h>
 
 /*	---	Memory pipe	---	*/
 int	**malloc_pipe(int num_pipes);
 void	free_pipe(int **pipes, int num_pipes);
 
 /*  ---  Pipe  ---  */
-int	ft_pipe(char ***commands, int num_pipes);
+int	ft_pipe(char ***commands, int num_pipes, t_minishell *mini);
 
 #endif
