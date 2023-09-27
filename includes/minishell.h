dd674150ceea1b851a3db6de26f48704063a6727
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-09-27 10:57:41 +0200

    Update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -9,34 +9,35 @@
 
 typedef struct s_minishell
 {
-    t_list      *env_list;
-    char        ***cmds;
-    char        *input;
-    int         fd_out;
-    int         og_out;
-    int         fd_in;
-    int         og_in;
-    int         err;
-    int         here_doc_number;
-    char        here_doc_name[200];
-    char        *here_doc_end;
+	t_list	*env_list;
+	char	***cmds;
+	char	*input;
+	int		**pipes;
+	int		fd_out;
+	int		og_out;
+	int		fd_in;
+	int		og_in;
+	int		err;
+	int		here_doc_number;
+	char	here_doc_name[200];
+	char	*here_doc_end;
 
-}   t_minishell;
+}	t_minishell;
 
 # define TEMP_FILE_NAME "/tmp/mini_temp_"
 
-int     print_error(char *err_description);
-char    ***get_cmds_value(char * input, t_list *env);
-void       execute_cmds(char ***cmds, t_list *env_list);
-void    parse_input(t_minishell *mini);
-void    delete_temp_files(t_minishell *mini);
-void    handle_here_document(t_minishell *mini, int i);
+int		print_error(char *err_description);
+char	***get_cmds_value(char * input, t_list *env);
+void	execute_cmds(char ***cmds, t_list *env_list);
+void	parse_input(t_minishell *mini);
+void	delete_temp_files(t_minishell *mini);
+void	handle_here_document(t_minishell *mini, int i);
 
 
 
 
-int    check_and_handle_redirections(char **cmds, t_minishell *mini);
-int close_redirections(t_minishell *mini);
+int		check_and_handle_redirections(char **cmds, t_minishell *mini);
+int		close_redirections(t_minishell *mini);
 
 
 
