b96bfa0094ca88437174772ce99c060ad687744e
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-09-27 11:18:17 +0200

    Update memory_pipe.c

--- src/pipe/memory_pipe.c
+++ src/pipe/memory_pipe.c
@@ -1,5 +1,15 @@
 #include "MS_pipe.h"
 
+void	free_pipe(int **pipes, int num_pipes)
+{
+	int	i;
+
+	i = -1;
+	while (++i < num_pipes)
+		free(pipes[i]);
+	free(pipes);
+}
+
 int	**malloc_pipe(int num_pipes)
 {
 	int	i;
@@ -17,19 +27,10 @@ int	**malloc_pipe(int num_pipes)
 		pipes[i] = (int *)malloc(2 * sizeof(int));
 		if (pipes[i] == NULL)
 		{
+			free_pipe(pipes, i);
 			perror("Error en malloc");
 			return (NULL);
 		}
 	}
 	return (pipes);
-}
-
-void	free_pipe(int **pipes, int num_pipes)
-{
-	int	i;
-
-	i = -1;
-	while (++i < num_pipes)
-		free(pipes[i]);
-	free(pipes);
 }
