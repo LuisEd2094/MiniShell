e522c9ae30f3facb3f99cdee586045b111e86e2e
Galo joel Macias zambrano <gmacias-@car8s1.42barcelona.com>
2023-10-02 08:57:11 +0200

    norminette ok

--- src/builtins/cd/ft_cd.c
+++ src/builtins/cd/ft_cd.c
@@ -1,39 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmacias- <gmacias-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 08:50:32 by gmacias-          #+#    #+#             */
+/*   Updated: 2023/10/02 08:52:29 by gmacias-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <builtins.h>
 
 int	check_home(char *home_directory)
-{	
-		if (home_directory == NULL)
-		{
-			perror("No se pudo obtener el directorio de inicio");
-			return -1;
-		}
-		if (chdir(home_directory) == -1)
-		{
-			perror("Error al cambiar al directorio de inicio");
-			return -1;
-		}
-	return 0;
+{
+	if (home_directory == NULL)
+	{
+		perror("No se pudo obtener el directorio de inicio");
+		return (-1);
+	}
+	if (chdir(home_directory) == -1)
+	{
+		perror("Error al cambiar al directorio de inicio");
+		return (-1);
+	}
+	return (0);
 }
 
 int	ft_cd(char **arguments)
 {
 	char	*home_directory;
-	
+
 	home_directory = getenv("HOME");
 	if (arguments == NULL)
-		return -1;
+		return (-1);
 	if (arguments[1] == NULL)
 	{
-		if(check_home(home_directory) == -1)
-			return -1;
+		if (check_home(home_directory) == -1)
+			return (-1);
 	}
 	else
 	{
 		if (chdir(arguments[1]) == -1)
 		{
 			perror("Error al cambiar de directorio");
-			return -1;
+			return (-1);
 		}
 	}
-	return 0;
+	return (0);
 }
--- src/builtins/echo/ft_echo.c
+++ src/builtins/echo/ft_echo.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmacias- <gmacias-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 08:52:45 by gmacias-          #+#    #+#             */
+/*   Updated: 2023/10/02 08:54:04 by gmacias-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <builtins.h>
 
 int	ft_flag_n(char *arguments)
@@ -25,13 +37,13 @@ int	ft_echo(char **arguments)
 	i = 0;
 	flag_n = 0;
 	if (arguments == NULL)
-		return -1;
+		return (-1);
 	if (arguments[1] != NULL && ft_flag_n(arguments[1]))
 	{
 		flag_n = 1;
 		i++;
 	}
-	while (arguments[++i] != NULL ) 
+	while (arguments[++i] != NULL)
 	{
 		ft_printf("%s", arguments[i]);
 		if (arguments[i] != NULL)
@@ -39,5 +51,5 @@ int	ft_echo(char **arguments)
 	}
 	if (flag_n)
 		ft_printf("\n");
-	return 0;
+	return (0);
 }
