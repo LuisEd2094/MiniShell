73e817549d85d14b7cdeb40a83bd4084700380cd
Luis <luissoto2094@gmail.com>
2023-10-04 15:40:56 +0200

    single quotes and valid strings should pass. Will check if string is empty before calling the function

--- test/mytest.c
+++ test/mytest.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libft.h>
+#include <stdbool.h>
 
 void setUp(void) {
     // set stuff up here
@@ -13,10 +14,43 @@ void tearDown(void) {
     // clean stuff up here
 }
 
+bool    parse_input(char *input);
+
+void check_basic_input(void)
+{
+    TEST_ASSERT_EQUAL(0, parse_input("Hola\n"));
+    TEST_ASSERT_EQUAL(0, parse_input("asdsdaasdajhdagjhdaghjsghajdghdaghadsghjadghjadshjgadsghjdaghjghjasdjhasjdh ajhgdasjhg a sdjhg dasjhd sahjgd jahgsdjg asdjgh ajhdga jhga jhg dajsgd ajg dashjg asjhd ahj\n"));
+    TEST_ASSERT_EQUAL(0, parse_input("[[AS[D[AD[DAS[D[AD[A[DAS[DA[SD[AS[D]]]]]]]]]]]]]\n"));
+    TEST_ASSERT_EQUAL(0, parse_input("adadadasdasdsadasdas\n"));
+}
+
+void check_quotes(void)
+{
+    TEST_ASSERT_EQUAL(0, parse_input("\"Hola\""));
+    TEST_ASSERT_EQUAL(1, parse_input("\"Hola"));
+
+    TEST_ASSERT_EQUAL(1, parse_input("\"Hola \" \" "));
+    TEST_ASSERT_EQUAL(0, parse_input("\"Hola \" \" \""));
+    TEST_ASSERT_EQUAL(0, parse_input("\"\"\"\""));
+
+
+
+    TEST_ASSERT_EQUAL(0, parse_input("\'Hola\'"));
+    TEST_ASSERT_EQUAL(1, parse_input("\'Hola"));
+
+    TEST_ASSERT_EQUAL(1, parse_input("\'Hola \' \' "));
+    TEST_ASSERT_EQUAL(0, parse_input("\'Hola \' \' \'"));
+    TEST_ASSERT_EQUAL(0, parse_input("\'\'\'\'"));
+
+
+
+}
+
 int main(int argc, char **argv, char **env)
 {  
     UNITY_BEGIN();
-    //RUN_TEST(get_argument_count_test);
+    RUN_TEST(check_basic_input);
+    RUN_TEST(check_quotes);
 
 
     return UNITY_END();
