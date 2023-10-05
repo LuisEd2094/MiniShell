daeb18cdb83c42279b1277e1a7cd989f2920a72d
gjmacias <ilsmaciaszambranogalo@gmail.com>
2023-10-05 14:09:16 +0200

    env

--- src/builtins/env/env.c
+++ src/builtins/env/env.c
@@ -1,9 +1,12 @@
 #include <builtins.h>
-
-void	ft_env(t_list *env_list)
+#include <shared.h>
+int	ft_print_env(char **cmds, t_list *env_list)
 {
 	t_list	*temp;
-
+	if (cmd_len(cmds) > 1)
+		return (print_error("env : invalid usage\n", 1));
+	if (!env_list->content)
+		return (0);
 	temp = env_list;
 	while (temp)
 	{
@@ -12,57 +15,58 @@ void	ft_env(t_list *env_list)
 		((t_env *)(temp->content))->value);
 		temp = temp->next;
 	}
+	return (0);
 }
-
 void	free_env_list(t_list *env_list)
 {
 	t_list	*next;
-
-	next = env_list->next;
-	while (env_list)
+	t_list	*cursor;
+	cursor = env_list;
+	while (cursor)
 	{
+		next = cursor->next;
 		free(((t_env *)(cursor->content))->variable);
 		free(((t_env *)(cursor->content))->value);
 		free((cursor->content));
 		free(cursor);
-		env_list = next;
+		cursor = next;
 	}
 }
-
-void	iter_env(t_list *env_node, char *env, t_list *tmp, t_list *env_list);
+t_list	*iter_env(char *env)
 {
 	t_list	*new;
-
-	env_node = create_env_node(env[i]);
+	t_env	*env_node;
+	env_node = create_env_node(env);
 	if (!env_node)
 		exit(1);
 	new = ft_lstnew(env_node);
 	if (!new)
 		exit (1);
-	tmp->next = new;
-	env_list->last = new;
-	tmp = new;
+	return (new);
 }
-
 t_list	*init_env(char **env)
 {
 	t_list	*env_list;
 	t_list	*tmp;
 	t_env	*env_node;
 	int		i;
-
-	if (!env || !env[0])
-        	return NULL;
-	env_node = create_env_node(env[0]);
+	if (!env[0])
+		env_node = NULL;
+	else
+		env_node = create_env_node(env[0]);
 	env_list = ft_lstnew(env_node);
 	if (!env_list)
 		exit(1);
 	env_list->last = env_list;
+	if (!env[0])
+		return(env_list);
 	tmp = env_list;
 	i = 1;
 	while (env[i])
 	{
-		iter_env(env_node, env[i], tmp,env_list);
+		tmp->next = iter_env(env[i]);
+		tmp = tmp->next;
+		env_list->last = tmp;
 		i++;
 	}
 	return (env_list);
