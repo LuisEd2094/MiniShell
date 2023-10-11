f097fca20eda7a9147c66ea955a89c7fb95f2ed2
Luis <luissoto2094@gmail.com>
2023-10-11 16:56:28 +0200

    now returns erros

--- src/builtins/exit/ft_exit.c
+++ src/builtins/exit/ft_exit.c
@@ -1,33 +1,49 @@
 #include <libft.h>
 #include <minishell.h>
 
+int is_only_num(char    *cmd)
+{
+    int i;
+
+    i = 0;
+    while (cmd[++i])
+    {
+        if (i == 0 && cmd[i] == '-')
+            i++;
+        else if (!ft_isdigit(cmd[i]))
+            return (0);
+    }
+    return (1);
+
+}
+
+void    print_only_numeric_error(char *cmd)
+{
+
+    write(STDERR_FILENO, "minishell: exit: ", ft_strlen("minishell: exit: "));
+    write(STDERR_FILENO, cmd[1], ft_strlen(cmd[1]));
+    write(STDERR_FILENO, ": numeric argument required\n", ft_strlen(": numeric argument required\n"));
+    exit (255);
+}
+
 int ft_exit(char **cmd, t_minishell *mini)
 {
     int i;
     int len;
 
     len = 0;
+    i = 0;
     while(cmd[len])
         len++;
     if (len > 2)
-    {  
-        ft_printf("Too many arguments\n");
-        return (1);
+    { 
+        if (!is_only_num(cmd[1]))
+            print_only_numeric_error(cmd[1]);
+        else
+            return (print_error("minishell: exit: too many arguments\n", 1));
     }
-    i = 0;
-    while(cmd[1] && cmd[1][i])
-    {
-        if (i == 0 && cmd[1][i] == '-')
-            i++;
-        else if (!ft_isdigit(cmd[1][i]))
-        {
-		    write(STDERR_FILENO, "minishell: exit: ", ft_strlen("minishell: exit: "));
-	        write(STDERR_FILENO, cmd[1], ft_strlen(cmd[1]));
-		    write(STDERR_FILENO, ": numeric argument required\n", ft_strlen(": numeric argument required\n"));
-            exit (255);
-        }
-        i++;
-    }
+    if (cmd[1] && !is_only_num(cmd[1]))
+        print_only_numeric_error(cmd[1]);
     if (cmd[1])
         exit(ft_atoi(cmd[1]));
     exit(EXIT_SUCCESS);
