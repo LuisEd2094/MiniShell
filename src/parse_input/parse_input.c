4cc868a3f5fe36bc7a1cf89c914a90464ffca711
Luis <luissoto2094@gmail.com>
2023-10-13 13:52:59 +0200

    parse input needs to pass norm

--- src/parse_input/parse_input.c
+++ src/parse_input/parse_input.c
@@ -1,115 +1,123 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsoto-do <lsoto-do@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 13:37:14 by lsoto-do          #+#    #+#             */
+/*   Updated: 2023/10/13 13:45:40 by lsoto-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "parse_input_internal.h"
 
-
-void init_checker(t_input *checker)
+void	init_checker(t_input *checker)
 {
-    checker->quote = 0;
-    checker->left_of_pipe = 0;
-    checker->pipe = 0;
-    checker->redirections = 0;
-    checker->redir_token = 0;
+	checker->quote = 0;
+	checker->left_of_pipe = 0;
+	checker->pipe = 0;
+	checker->redirections = 0;
+	checker->redir_token = 0;
 }
 
-int     print_input_error(char *str, char *error, int code)
+int	print_input_error(char *str, char *error, int code)
 {
-    print_error(str, code);
-    if (error)
-    {
-        if (error[1])
-        {
-            if (error[0] == '|' && error[1] == '|')
-                print_error("||", code);
-            else if (error[0] == '|')
-                print_error("|", code);
-            else if (error[0] == '>' && error[1] == '>')
-                print_error(">>", code);
-            else if (error[0] == '>')
-                print_error(">", code);
-            else if (error[0] == '<')
-            {
-                if (error[2] && error[1] == '<' && error[2] == '<' )
-                        print_error("<<<", code);
-                else if (error[0] == '<' && error[1] == '<')
-                    print_error("<<", code);
-            }
-        }
-        else
-            print_error(error, code);
-    }
-    print_error("\n", code);
-    return (code);
+	print_error(str, code);
+	if (error)
+	{
+		if (error[1])
+		{
+			if (error[0] == '|' && error[1] == '|')
+				print_error("||", code);
+			else if (error[0] == '|')
+				print_error("|", code);
+			else if (error[0] == '>' && error[1] == '>')
+				print_error(">>", code);
+			else if (error[0] == '>')
+				print_error(">", code);
+			else if (error[0] == '<')
+			{
+				if (error[2] && error[1] == '<' && error[2] == '<' )
+					print_error("<<<", code);
+				else if (error[0] == '<' && error[1] == '<')
+					print_error("<<", code);
+			}
+		}
+		else
+			print_error(error, code);
+	}
+	return (print_error("\n", code));
 }
 
-int    check_vals(t_input *checker)
+int	check_vals(t_input *checker)
 {
-    if (checker->quote)
-        return(print_input_error(QUOTES , NULL, 1));
-    if (checker->pipe)
-        return(print_input_error(PIPES, NULL, 1));
-    if (checker->redirections)
-        return(print_input_error(UNEXPECTED, "'newline'", 258));
-    return (0);
+	if (checker->quote)
+		return (print_input_error(QUOTES, NULL, 1));
+	if (checker->pipe)
+		return (print_input_error(PIPES, NULL, 1));
+	if (checker->redirections)
+		return (print_input_error(UNEXPECTED, "'newline'", 258));
+	return (0);
 }
 
-int    parse_input(char *input)
+int	parse_input(char *input)
 {
-    t_input checker;
-    int     i;
+	t_input	checker;
+	int		i;
 
-    init_checker(&checker);
-    i = -1;
-    while (input[++i])
-    {
-        if (ft_isspace(input[i]))
-               continue;
-        if (!checker.redirections && !checker.quote && (input[i] == '>' || input[i] == '<'))
-        {
-            checker.redirections = 1;
-            checker.redir_token = input[i];
-            checker.redir_pos = i;
-        }
-        else if (checker.redirections)
-        {
-            if (input[checker.redir_pos + 1] && input[checker.redir_pos] == input[checker.redir_pos + 1] && \
-            ((input[i] == '>' || input[i] == '<') && i != checker.redir_pos + 1))
-                return(print_input_error(UNEXPECTED, &input[i], 258));
-            if (i == checker.redir_pos + 1 && (input[i] == '|' || (input[i] == '>' || input[i] == '<')))
-            {
-                if (checker.redir_token == '>' && input[i] == '<')
-                    return(print_input_error(UNEXPECTED, &input[i], 258));
-                continue;
-            }
-            else
-            {
-                if (input[i] == '>' || input[i] == '<')
-                    return(print_input_error(UNEXPECTED, &input[i], 258));
-            }
-            if (input[i] !=  '|')
-                checker.redirections = 0;
-        }
-        if (!checker.left_of_pipe && input[i] != '|')
-            checker.left_of_pipe = 1;
-        if (!checker.quote && (input [i] == '\'' || input[i] == '"'))
-            checker.quote = input[i];
-        else if (checker.quote)
-        {
-            if (input[i] == checker.quote)
-                checker.quote = 0;
-        }
-        if (!checker.pipe && input[i] == '|')
-        {
-            if (!checker.left_of_pipe)
-                return(print_input_error(UNEXPECTED, &input[i], 258));
-            checker.pipe = 1;
-        }
-        else if (checker.pipe)
-        {
-            if (input[i] == '|')
-                return(print_input_error(UNEXPECTED, &input[i], 258));
-            checker.pipe = 0;
-        }
-    }
-    return check_vals(&checker);
+	init_checker(&checker);
+	i = -1;
+	while (input[++i])
+	{
+		if (ft_isspace(input[i]))
+			   continue;
+		if (!checker.redirections && !checker.quote && (input[i] == '>' || input[i] == '<'))
+		{
+			checker.redirections = 1;
+			checker.redir_token = input[i];
+			checker.redir_pos = i;
+		}
+		else if (checker.redirections)
+		{
+			if (input[checker.redir_pos + 1] && input[checker.redir_pos] == input[checker.redir_pos + 1] && \
+			((input[i] == '>' || input[i] == '<') && i != checker.redir_pos + 1))
+				return(print_input_error(UNEXPECTED, &input[i], 258));
+			if (i == checker.redir_pos + 1 && (input[i] == '|' || (input[i] == '>' || input[i] == '<')))
+			{
+				if (checker.redir_token == '>' && input[i] == '<')
+					return(print_input_error(UNEXPECTED, &input[i], 258));
+				continue;
+			}
+			else
+			{
+				if (input[i] == '>' || input[i] == '<')
+					return(print_input_error(UNEXPECTED, &input[i], 258));
+			}
+			if (input[i] !=  '|')
+				checker.redirections = 0;
+		}
+		if (!checker.left_of_pipe && input[i] != '|')
+			checker.left_of_pipe = 1;
+		if (!checker.quote && (input [i] == '\'' || input[i] == '"'))
+			checker.quote = input[i];
+		else if (checker.quote)
+		{
+			if (input[i] == checker.quote)
+				checker.quote = 0;
+		}
+		if (!checker.pipe && input[i] == '|')
+		{
+			if (!checker.left_of_pipe)
+				return(print_input_error(UNEXPECTED, &input[i], 258));
+			checker.pipe = 1;
+		}
+		else if (checker.pipe)
+		{
+			if (input[i] == '|')
+				return(print_input_error(UNEXPECTED, &input[i], 258));
+			checker.pipe = 0;
+		}
+	}
+	return check_vals(&checker);
 }
--- src/parse_input/parse_input_internal.h
+++ src/parse_input/parse_input_internal.h
@@ -1,23 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_input_internal.h                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsoto-do <lsoto-do@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 13:45:50 by lsoto-do          #+#    #+#             */
+/*   Updated: 2023/10/13 13:52:44 by lsoto-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PARSE_INPUT_INTERNAL_H
 # define PARSE_INPUT_INTERNAL_H
 # include <libft.h>
 # include <stdbool.h>
 # include <shared.h>
 
-#define UNEXPECTED "minishell: syntax error near unexpected token "
-#define QUOTES "minishell: syntax error, unclosed quote"
-#define PIPES  "minishell: syntax error, unclosed pipe"
+# define UNEXPECTED "minishell: syntax error near unexpected token "
+# define QUOTES "minishell: syntax error, unclosed quote"
+# define PIPES  "minishell: syntax error, unclosed pipe"
 
 typedef struct s_input
 {
-    char    quote;
-    char    redir_token;
-    int     redir_pos;
-    bool    pipe;
-    bool    left_of_pipe;
-    bool    redirections;
-}   t_input;
+	char	quote;
+	char	redir_token;
+	int		redir_pos;
+	bool	pipe;
+	bool	left_of_pipe;
+	bool	redirections;
+}	t_input;
 
 #endif
-
-
