5a0ff5e4a7f97f6646fd133b98f082164603137d
Luis <luissoto2094@gmail.com>
2023-10-14 13:16:46 +0200

    adds one to shlvl at init and throws error if it goes over 999

--- src/builtins/env/env_aux.c
+++ src/builtins/env/env_aux.c
@@ -12,12 +12,48 @@
 
 #include "env_internal.h"
 
+void	print_shlvl_high(char *lvl)
+{
+	write(STDERR_FILENO,"minishell: warning: shell level (", \
+	ft_strlen("minishell: warning: shell level ("));
+	write(STDERR_FILENO, lvl, ft_strlen(lvl));
+	write(STDERR_FILENO,") too high, resetting to 1\n", \
+	ft_strlen(") too high, resetting to 1\n"));
+}
+
+t_env	*check_shlvl(t_env	*node)
+{
+	int		level;
+	char	*str;
+
+	if (ft_strcmp(node->variable, "SHLVL") == 0)
+	{
+		level = ft_atoi(node->value);
+		free(node->value);
+		level++;
+		if (level >= 1000)
+		{
+			str = ft_itoa(level);
+			print_shlvl_high(str);
+			free(str);
+			level = 0;
+		}
+		node->value = ft_itoa(level);
+		if (!node->value)
+			return (NULL);
+	}
+	return (node);
+}
+
 t_list	*iter_env(char *env)
 {
 	t_list	*new;
 	t_env	*env_node;
 
 	env_node = create_new_env_node(env);
+	if (!env_node)
+		return (NULL);
+	env_node = check_shlvl(env_node);
 	if (!env_node)
 		return (NULL);
 	new = ft_lstnew(env_node);
