6a581841529766044195e486f62d3dfd523f68dc
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-10-16 20:01:53 +0200

    Update ft_cd.c

--- src/builtins/cd/ft_cd.c
+++ src/builtins/cd/ft_cd.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+ /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
@@ -6,12 +6,32 @@
 /*   By: gmacias- <gmacias-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 08:50:32 by gmacias-          #+#    #+#             */
-/*   Updated: 2023/10/13 11:12:08 by lsoto-do         ###   ########.fr       */
+/*   Updated: 2023/10/05 14:51:47 by galo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <builtins.h>
-#include <shared.h>
+
+int	send_old_directory(t_list *env_list, char *sol)
+{
+	char	*old_directory;
+	
+	old_directory = malloc((ft_strlen("OLDPWD") + 1) * sizeof(char));
+	if (old_directory == NULL)
+	{
+		perror("Error OLDPWD: Fallo en malloc");
+		return (3);
+	}
+	if (sol == NULL)
+	{
+		free(old_directory);
+		perror("Error OLDPWD: Fallo en malloc");
+		return (3);
+	}
+	ft_strlcpy(old_directory, "OLDPWD", ft_strlen("OLDPWD"));
+	create_or_update_env_node(env_list, old_directory, sol);
+	return (0);
+}
 
 int	change_directory(char *directory)
 {
@@ -28,23 +48,46 @@ int	change_directory(char *directory)
 	return (0);
 }
 
+char	*expand_tilde(char *path) 
+{
+	char	*expanded_path;
+	size_t	expanded_size;
+
+	if (getenv("HOME") == NULL)
+	{
+		perror("No se pudo obtener el directorio");
+		return (NULL);
+	}
+	expanded_size = ft_strlen(getenv("HOME")) + ft_strlen(path);
+	expanded_path = malloc(expanded_size * sizeof(char));
+	ft_strlcpy(expanded_path, getenv("HOME"), expanded_size);
+	ft_strlcat(expanded_path, path + 1, expanded_size);
+	return (expanded_path);
+}
+
 int	ft_cd(char **arguments, t_list *env_list)
 {
 	char	*home_directory;
+	char	*tilde_directory;
 	char	*save_old_directory;
 	int		error;
 
 	error = 0;
 	home_directory = getenv("HOME");
-	if (arguments == NULL)
-		return (-1);
 	save_old_directory = getcwd(NULL, 0);
 	if (arguments[1] == NULL)
 		error = change_directory(home_directory);
+	else if (arguments[1][0] != '\0' && arguments[1][0] == '-' && arguments[1][1] == '\0')
+		error = change_directory(save_old_directory);
+	else if (arguments[1][0] == '~')
+	{
+		tilde_directory = expand_tilde(arguments[1]);
+		error = change_directory(tilde_directory);
+		free(tilde_directory);
+	}
 	else
 		error = change_directory(arguments[1]);
 	if (error == 0)
-		create_or_update_env_node(env_list, "OLDPWD", save_old_directory);
-	free(save_old_directory);
+		error = send_old_directory(env_list, save_old_directory);
 	return (error);
 }
