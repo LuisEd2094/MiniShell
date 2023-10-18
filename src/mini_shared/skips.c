fbe8d17b6e6a191b66d6ee914b5462290777e8bf
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-10-18 12:32:25 +0200

    Update skips.c

--- src/mini_shared/skips.c
+++ src/mini_shared/skips.c
@@ -42,7 +42,7 @@ int	get_arg_size_skip_redirections(char *input)
 
 	i = 0;
 	while (input[i] && is_ascii_no_space(input[i]) || \
-	input[i] == '>' || input[i] == '<'))
+	!(input[i] == '>' || input[i] == '<'))
 		i++;
 	return (i);
 }
