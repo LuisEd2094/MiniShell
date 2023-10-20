9ed6be36cd6ebc5607e85a9a9c9f4eb842540b14
Luis <luissoto2094@gmail.com>
2023-10-20 12:40:33 +0200

    exit passes more

--- infinito.sh
+++
@@ -1,7 +0,0 @@
-i=0
-
-while true; do
-    i=$((i+1))
-    echo "$i second(s) passed"
-    sleep 1
-done
--- valgrind.sh
+++
@@ -1,1 +0,0 @@
-valgrind --suppressions=readline.supp --track-origins=yes --leak-check=full --show-leak-kinds=all ./minishell
